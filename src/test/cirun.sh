c99404aed1f6e2565f5786998fe286dd4b0aae25
Ramesh Babu <rameshbabu.thoomu@gmail.com>
2017-05-16 00:20:27 -0400

    Update cirun.sh

--- src/test/cirun.sh
+++ src/test/cirun.sh
@@ -12,7 +12,7 @@ export ORG_HYPERLEDGER_FABRIC_SDK_LOGLEVEL=TRACE
 export ORG_HYPERLEDGER_FABRIC_CA_SDK_LOGLEVEL=TRACE
 
 cd $WD/src/test/fixture/sdkintegration
-./fabric.sh restart >dockerlogfile.log 2>&1 &
+#./fabric.sh restart >dockerlogfile.log 2>&1 &
 cd $WD
 sleep 30
 docker ps -a
